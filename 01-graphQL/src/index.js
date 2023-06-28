7732e7061b10efeb001ac9629b3e6ed4d1536d44
GiltMonster <lucassantoslukas2@gmail.com>
2023-06-28 02:10:19 -0300

    manipulando os tipos escalares

--- 01-graphQL/src/index.js
+++ 01-graphQL/src/index.js
@@ -1,24 +1,28 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+//Para obrigar o objeto a nao ser null, basta colocar uma '!' ao lado
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
-      hello: String
-      hi: String
+      id: ID
+      name: String
+      age: Int
+      average: Float
+      married: Boolean!
+      arrayString: [String!]!
     }
   `,
   resolvers: {
     Query: {
-      hello: () => {
-        return 'hello again';
-      },
-      hi: () => {
-        return 'Hi';
-      },
+      id: () => '1a2diady7-sd6',
+      name: () => 'Lucas S. campos',
+      age: () => 22,
+      average: () => 50.55,
+      married: () => false,
+      arrayString: () => ['a', 'b', 'c'],
     },
   },
 });
-
 server.listen(4000).then(({ url }) => {
   console.log(`Server listening on url: ${url}`);
 });
