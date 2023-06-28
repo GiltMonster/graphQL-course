de224eebf8ce7bb34e959233afcb516ec52fa1c7
GiltMonster <lucassantoslukas2@gmail.com>
2023-06-28 01:51:53 -0300

    Aprendendo a manipular o typeDefs(com o gql) e resolver

--- 01-graphQL/src/index.js
+++ 01-graphQL/src/index.js
@@ -1,1 +1,24 @@
-console.log('Hello Word 01');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+      hi: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => {
+        return 'hello again';
+      },
+      hi: () => {
+        return 'Hi';
+      },
+    },
+  },
+});
+
+server.listen(4000).then(({ url }) => {
+  console.log(`Server listening on url: ${url}`);
+});
