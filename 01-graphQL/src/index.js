b01264cfb66b9471f032bc6249490983a77f1032
GiltMonster <lucassantoslukas2@gmail.com>
2023-06-28 03:05:43 -0300

    Modularizei o schema

--- 01-graphQL/src/index.js
+++ 01-graphQL/src/index.js
@@ -1,48 +1,10 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import { typeDefs, resolvers } from './graphql/schema';
 
 //Para obrigar o objeto a nao ser null, basta colocar uma '!' ao lado
 const server = new ApolloServer({
-  typeDefs: gql`
-    type Query {
-      user: User!
-      users: [User!]!
-    }
-
-    type User {
-      id: ID!
-      userName: String!
-    }
-  `,
-  resolvers: {
-    Query: {
-      user: () => {
-        return {
-          id: 'klj234iol2-2n',
-          userName: 'Lucas S.',
-        };
-      },
-      users: () => {
-        return [
-          {
-            id: 'klj234iol2-2n',
-            userName: 'Lucas S.',
-          },
-          {
-            id: 'dfsgsde3112-2n',
-            userName: 'Paggnota',
-          },
-          {
-            id: 'sadfqw2123-2n',
-            userName: 'TH',
-          },
-          {
-            id: '1dsfgdsfgew42-2n',
-            userName: 'Luis',
-          },
-        ];
-      },
-    },
-  },
+  typeDefs,
+  resolvers,
 });
 server.listen(4000).then(({ url }) => {
   console.log(`Server listening on url: ${url}`);
